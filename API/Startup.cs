69d7a65c20773ea70c914a4e30fd98958edecc46
Raeen <Raeen@DESKTOP-BQR2SU1>
2022-11-16 10:54:34 +0330

    First get data from bakend with front

--- API/Startup.cs
+++ API/Startup.cs
@@ -20,22 +20,32 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                 {
+                     policy
+                     .AllowAnyMethod()
+                     .AllowAnyHeader()
+                     .WithOrigins("http://localhost:3000", "http://192.168.1.2:3000", "https://localhost:5001");
+                 });
+            });
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
-            //app.UseHttpsRedirection();
-           
+            // app.UseHttpsRedirection();
+
             app.UseRouting();
-            
+
             app.UseAuthorization();
-            
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
